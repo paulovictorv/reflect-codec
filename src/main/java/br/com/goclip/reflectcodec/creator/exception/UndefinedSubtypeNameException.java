b46bc777df29d39796404bb77d1a7b192ccfb2ad
jeovane da costa <jeovanebc@gmail.com>
2020-01-30 15:04:15 -0300

    improvements after MR comments

--- src/main/java/br/com/goclip/reflectcodec/creator/exception/UndefinedSubtypeNameException.java
+++ src/main/java/br/com/goclip/reflectcodec/creator/exception/UndefinedSubtypeNameException.java
@@ -2,6 +2,6 @@ package br.com.goclip.reflectcodec.creator.exception;
 
 public class UndefinedSubtypeNameException extends RuntimeException {
     public UndefinedSubtypeNameException(Class<?> type) {
-        super(String.format("No defined subtype key ID for subclass %s", type.getSimpleName()));
+        super(String.format("No defined subtype key ID for subclass %s. Refer to Jackson's documentation on polymorphic deserialization.", type.getSimpleName()));
     }
 }
