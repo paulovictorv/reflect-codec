99961f41440d1b47619d5b83f04326aad49a7062
paulo_melo <paulo@goclip.com.br>
2021-10-31 17:23:02 -0300

    refactoring exception, encapsulating error message generation

--- src/main/java/io/pmelo/reflectcodec/creator/exception/UnsupportedCollectionException.java
+++ src/main/java/io/pmelo/reflectcodec/creator/exception/UnsupportedCollectionException.java
@@ -2,7 +2,8 @@ package io.pmelo.reflectcodec.creator.exception;
 
 public class UnsupportedCollectionException extends RuntimeException {
 
-    public UnsupportedCollectionException(String message) {
-        super(message);
+    public UnsupportedCollectionException(Class<?> unsupportedCollection) {
+        super(String.format("Unsupported collection: %s. Make sure that it has an empty constructor.",
+                unsupportedCollection.getSimpleName()));
     }
 }
